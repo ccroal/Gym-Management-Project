f674acc1d58f7f9a8c548489b9e23c4bf8299194
Christopher Croal <ccroal@ntlworld.com>
2018-12-08 17:01:57 +0000

    added show all members in members index.erb

--- controller.rb
+++ controller.rb
@@ -1,7 +1,7 @@
 require('sinatra')
 require('sinatra/contrib/all')
 
-require_relative('./controllers.member_controller.rb')
+require_relative('./controllers/member_controller.rb')
 
 get '/' do
   erb(:index)
--- controllers/member_controller.rb
+++ controllers/member_controller.rb
@@ -1,4 +1,7 @@
-require('sinatra')
-require('sinatra/contrib/all')
 require_relative('../models/member.rb')
 also_reload('..models/*')
+
+get '/members' do
+  @members = Member.all
+  erb(:"members/index")
+end
