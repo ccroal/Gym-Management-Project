44b2513c002ad32e5c38f3f6ded603a23e472024
Christopher Croal <ccroal@ntlworld.com>
2018-12-08 17:42:40 +0000

    Created add new member page new.erb

--- controllers/member_controller.rb
+++ controllers/member_controller.rb
@@ -6,6 +6,10 @@ get '/members' do
   erb(:"members/index")
 end
 
+get '/members/new' do
+  erb(:"members/new")
+end
+
 get '/members/:id' do
   @member = Member.find(params['id'])
   erb(:"members/show")
