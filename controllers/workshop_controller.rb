f4708d0ba59b8a671aa3c3c1b3ad2b072e0464f0
Christopher Croal <ccroal@ntlworld.com>
2018-12-09 12:24:52 +0000

    added create new workshop to site

--- controllers/workshop_controller.rb
+++ controllers/workshop_controller.rb
@@ -5,3 +5,12 @@ get '/workshops' do
   @workshops = Workshop.all
   erb(:"workshops/index")
 end
+
+get '/workshops/new' do
+  erb(:"workshops/new")
+end
+
+post '/workshops' do
+  Workshop.new(params).save
+  redirect to '/workshops'
+end
